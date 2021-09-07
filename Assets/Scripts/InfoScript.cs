fe9f718a57d3881155bf12bc4623cca5d9567fac
Dhruv <dhruv18999@gmail.com>
2021-09-07 15:13:02 +0530

    Updates Done

--- Assets/Scripts/InfoScript.cs
+++ Assets/Scripts/InfoScript.cs
@@ -6,24 +6,22 @@ using UnityEngine.XR.ARFoundation;
 
 public class InfoScript : MonoBehaviour
 {
-    public static Text infoTxt;
-    public static Image img;
-
+ 
+    public static Camera camera;
+    public Camera cam1;
     public Text t;
     public Image i;
 
     static bool detection = false;
-    static bool tapOn1 = false;
-    static bool tapOn2 = false;
+ 
 
     public static ARTrackedImageManager aRTrackedImageManager;
     public ARTrackedImageManager mang;
 
     void Start()
     {
-        infoTxt = t;
-        img = i;
         aRTrackedImageManager = mang;
+        camera = cam1;
     }
 
     // Update is called once per frame
@@ -32,26 +30,10 @@ public class InfoScript : MonoBehaviour
         if (!detection && aRTrackedImageManager.trackables.count == 1)
         {
             detection = true;
-            infoTxt.text = "Image is tracked! Click on coloured sphere to change color";
+            i.gameObject.SetActive(false);
+            t.gameObject.SetActive(false);
         }
     }
 
-    public static void SP1()
-    {
-        if (!tapOn1)
-        {
-            tapOn1 = true;
-            infoTxt.text = "Click on other sphere to rotate it! First Click will rotate it and second click will stop it";
-        }
-    }
-
-    public static void SP2()
-    {
-        if (!tapOn2)
-        {
-            infoTxt.gameObject.SetActive(false);
-            tapOn2 = true;
-            img.gameObject.SetActive(false);
-        }
-    }
+   
 }
--- Assets/Scripts/interaction.cs
+++ Assets/Scripts/interaction.cs
@@ -1,17 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class interaction : MonoBehaviour
 {
     public GameObject sphere_1;
     public GameObject sphere_2;
 
+    public Text txt1;
+    public Text txt2;
+
+    public Canvas canvas1;
+    public Canvas canvas2;
+
+    bool sp1First = false;
+    int sp2Click = 0;
+
     public AudioSource Audio;
     public AudioClip sound_1;
     public AudioClip sound_2;
 
     bool spinSp2 = false;
+
+    void Start()
+    {
+        canvas1.worldCamera = InfoScript.camera;
+        canvas2.worldCamera = InfoScript.camera;
+    }
 
     // Update is called once per frame
     void Update()
@@ -43,16 +59,32 @@ public class interaction : MonoBehaviour
 
     public void onClickSp1()
     {
-        InfoScript.SP1();
         Audio.PlayOneShot(sound_1);
         //Below line will chnage material color to new Random color
         sphere_1.GetComponent<Renderer>().material.color = new Color(Random.value, Random.value, Random.value, 1.0f);
+
+        if(!sp1First)
+        {
+            sp1First = true;
+            canvas1.gameObject.SetActive(false);
+        }
     }
 
     public void onClickSp2()
     {
-        InfoScript.SP2();
         Audio.PlayOneShot(sound_2);
         spinSp2 = !spinSp2;
+
+        if(sp2Click == 0)
+        {
+            sp2Click++;
+            txt2.text = "Click Again to stop rotating";
+        }
+        else if(sp2Click == 1)
+        {
+            canvas2.gameObject.SetActive(false);
+            sp2Click = -1;
+
+        }
     }
 }
